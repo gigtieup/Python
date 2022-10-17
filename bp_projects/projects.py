a36ffa0eb54b3b5dce4a7e3ed489e4b56bd5d32f
gigtieup <quinnbireley3.14@gmail.com>
2022-10-17 15:15:43 -0700

    #sjsjs

--- bp_projects/projects.py
+++ bp_projects/projects.py
@@ -21,3 +21,47 @@ def walruses():
 @app_projects.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
+
+@app_projects.route('/shirt/')
+def shirt():
+    return render_template("shirt.html")
+
+@app_projects.route('/options/')
+def options():
+    return render_template("options.html")
+
+@app_projects.route('/pants/')
+def pants():
+    return render_template("pants.html")
+
+@app_projects.route('/co/')
+def co():
+    return render_template("co.html")
+
+@app_projects.route('/jew/')
+def jew():
+    return render_template("jew.html")
+
+@app_projects.route('/sighnin/')
+def sighnin():
+    return render_template("sighnin.html")
+
+@app_projects.route('/welcome/')
+def welcome():
+    return render_template("welcome.html")
+
+@app_projects.route('/cart/')
+def cart():
+    return render_template("cart.html")
+
+@app_projects.route('/cart2/')
+def cart2():
+    return render_template("cart2.html")
+
+@app_projects.route('/cart3/')
+def cart3():
+    return render_template("cart3.html")
+
+@app_projects.route('/wow/')
+def wow():
+    return render_template("wow.html")
