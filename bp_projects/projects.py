f44455cfa808d2f8da96925dd5d52416586acca6
gigtieup <quinnbireley3.14@gmail.com>
2022-10-21 14:00:58 -0700

    #d

--- bp_projects/projects.py
+++ bp_projects/projects.py
@@ -61,7 +61,3 @@ def cart2():
 @app_projects.route('/cart3/')
 def cart3():
     return render_template("cart3.html")
-
-    @app_projects.route('/02_API/')
-def API():
-    return render_template("API.md")
