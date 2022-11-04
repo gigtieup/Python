9363c64c6dc167b97862d50d8a682ec3fca8051c
aaron-rub <aaronr06138@gmail.com>
2022-11-04 14:25:49 -0700

    chanegerjfdkj

--- api.py
+++ api.py
@@ -61,8 +61,8 @@ class JokesAPI:
     api.add_resource(_UpdateJeer, '/jeer/<int:id>')
     
 if __name__ == "__main__":
-    # server = "http://127.0.0.1:5000" # run local
-    server = 'https://flask.nighthawkcodingsociety.com' # run from web
+    server = "http://127.0.0.1:5000" # run local
+    # server = 'https://flask.nighthawkcodingsociety.com' # run from web
     url = server + "/api/jokes"
     responses = []  # responses list
 
